8b3112b99b5ad026da9c40df036a037e0c41a0a1
Srinivasa Rao A <srinivasarao.adapa1@wipro.com>
2021-08-05 00:21:06 +0100

    Added types for component App

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,9 @@
+import { FC, ReactElement } from 'react';
 import './App.css';
 import Toggle from './Components/Toggle';
 import Parent from './Components/Parent';
 
-const App = () => (
+const App: FC = (): ReactElement => (
   <div className="columns">
     <div className="column">
       <Toggle/>
